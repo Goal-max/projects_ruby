9ca1ff79c296ec2b7b207dee337e361afb860737
rain <rain@debian.gateway>
2026-06-03 20:45:36 +1000

    working for multiple words but fails case-insensitive

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,13 +1,16 @@
 dictionary = ["below", "down", "go", "horn", "how", "howdy", "it", "I", "low",
 "own", "part", "partner", "sit"]
 def substrings (string, dictionary)
+  array_of_strings = string.split(' ')
   list = {}
   dictionary.each do |word| 
-    if string.include?(word)
-      if list.include?(word)
-        list[word] = list[word] + 1
-      else
-        list[word] = 1
+    array_of_strings.each do |element| 
+      if element.include?(word)
+        if list.include?(word)
+          list[word] = list[word] + 1
+        else
+          list[word] = 1
+        end
       end
     end
   end
@@ -15,3 +18,5 @@ def substrings (string, dictionary)
 end 
 
 puts substrings("below", dictionary)
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
