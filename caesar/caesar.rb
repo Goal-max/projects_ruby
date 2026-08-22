69f55354c7c9266a8e7596e2c51d35c15af1c175
rain <rain@debian.gateway>
2026-08-23 09:38:05 +1000

    Refactor
    Simpifies code as there was too much repeat code

--- caesar/caesar.rb
+++ caesar/caesar.rb
@@ -8,39 +8,28 @@ def caesar_cipher(string, number)
 end
 
 def shift_character(char, number)
-  if !/[A-Za-z]/.match?(char)
-    char
-  elsif upcase?(char)
-    upcase_char_ord(char, number).chr
+  if /[A-Z]/.match?(char)
+    char = char.downcase
+    new_letter(char, number).upcase
+  elsif /[a-z]/.match?(char)
+    new_letter(char, number)
   else
-    lowcase_char_ord(char, number).chr
+    char
   end
 end
 
-def upcase?(character)
-  character.upcase == character
-end
-
-def upcase_char_ord(character, number)
-  new_character_ordinal = character.ord + number
-  if new_character_ordinal > 'Z'.ord
-    corrected_character_ordinal = new_character_ordinal - 'Z'.ord + 64
-    return corrected_character_ordinal
+def new_letter(character, number)
+  new_letter_ordinal = character.ord + number
+  z_ordinal = 'z'.ord
+  if new_letter_ordinal > z_ordinal
+    corrected_character_ordinal = new_letter_ordinal - z_ordinal + 96
+    return corrected_character_ordinal.chr
   end
-  new_character_ordinal
-end
-
-def lowcase_char_ord(character, number)
-  new_character_ordinal = character.ord + number
-  if new_character_ordinal > 'z'.ord
-    corrected_character_ordinal = new_character_ordinal - 'z'.ord + 96
-    return corrected_character_ordinal
-  end
-  new_character_ordinal
+  new_letter_ordinal.chr
 end
 
 string = 'What a string!'
 shift_factor = 5
 puts "string is: #{string}"
 puts "shift factor is: #{shift_factor}"
-puts " deciphered string: #{caesar_cipher(string, shift_factor)}"
+puts "deciphered string: #{caesar_cipher(string, shift_factor)}"
