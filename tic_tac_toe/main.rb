9b09625db6d0557aa71fca1fcf572d1570e1246a
rain <rain@debian.gateway>
2026-08-30 05:33:00 +1000

    refactor

--- tic_tac_toe/main.rb
+++ tic_tac_toe/main.rb
@@ -18,11 +18,7 @@ end
 
 def diagonal_wins(rows, columns)
   hash = {}
-  # array_indices = [0, 1, 2]
-  # column_indices = [[array_indices], [array_indices.reverse]]
   rows.each_index do |index|
-    # column_indices.each_index do |outer_array|
-    # outer_array.each_index do |column_index|
     hash[rows[index]] = columns[index]
   end
   hash
@@ -34,7 +30,6 @@ columns = %w[left middle right]
 winning = {
   'horizontal_wins' => straight_wins(rows, columns),
   'vertical_wins' => straight_wins(columns, rows),
-  # diagonal wins
   'diagonal_wins' => [diagonal_wins(rows, columns),
                       diagonal_wins(rows, columns.reverse)]
 }
