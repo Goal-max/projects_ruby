36b85aed6d338e24445ad6913f1ba8fae6e0fcac
rain <rain@debian.gateway>
2026-08-29 17:17:14 +1000

    WIP: added more paths for winning

--- tic_tac_toe/main.rb
+++ tic_tac_toe/main.rb
@@ -4,8 +4,15 @@ require_relative 'lib/board'
 board = Board.new
 board.choice('top', 'middle', 'peter')
 
+
+rows = %w[top middle bottom]
+columns = %w[left middle right]
+
 winning = {
-  'top' => %w[left middle right]
+  horizontal_wins => straight_wins(rows, columns),
+  vertical_wins => straight_wins(columns, rows),
+  # diagonal wins
+  'left' => [top],
 }
 
 # find names at each of 3 winning positions
@@ -15,6 +22,22 @@ winning.each_pair do |key, value|
     array << board.board_info.dig(key, column)
   end
   puts "array is #{array}"
+end
+
+def check_win?(array, player_name)
+  array.all?(player_name)
+end
+
+def straight_wins(position1, position2)
+  hash = {}
+  position1.each do |element|
+    hash[element] = position2
+  end
+  hash
+end
+
+def diagonal_wins(rows, columns)
+  rows.each_index
 end
 
 binding.b
