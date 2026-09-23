4ab95ded93049b72f37bcd1bf340094e5d8091ba
rain <rain@debian.gateway>
2026-09-24 06:34:02 +1000

    WIP

--- master_mind/lib/computer_code_breaker.rb
+++ master_mind/lib/computer_code_breaker.rb
@@ -1,24 +1,45 @@
 class Computer
   attr_reader :colours
+  attr_accessor :colour_patterns
 
   def initialize(board, colours)
     @board = board
-    @colours = create_colour_pair(colours)
+    @colours = colours
+    @colour_patterns
   end
 
+=begin
   def create_colour_pairs(colours)
     colours_copy = colours
     colour_pairs = []
     colour_pairs << colours_copy.shift(2) until colours_copy.empty?
     colour_pairs
   end
-  
-  def create_guess(pair)
-    @colour_pair = colours[0]
-    pattern = [0, 1, 0, 1]
+=end
+
+  colours_indices = colours.each_index.map do |index|
+    index
+  end
+  until colours_indices.empty?
+    index_pair = colours_indices.shift(2)
+    first_index = index_pair[0]
+    second_index = index_pair[1]
+    create_pattern(first_index, second_index)
+  end
+
+  def create_first_pattern(first_index, second_index)
+    [first_index, first_index, second_index, second_index]
+  end
+
+# create below only if above >= 1 red/white. Unshift onto array patterns list
+  def create_second_pattern(first_index)
+    [first_index, first_index, first_index, first_index]
+  end
+
+  def create_guess(index_pattern)
     guess = []
-    pattern.each do |element|
-      guess << colour_pair[element]
+    index_pattern.each do |element|
+      guess << colours[element]
     end
     guess
   end
